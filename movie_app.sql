607d5799a9492b186af7f10780272269cfa969e1
Marwan Khaled <merokhaled708@gmail.com>
2026-04-23 18:08:48 +0200

    Change id column type to varchar in movies table
    Changed the id column type from int to varchar.

--- movie_app.sql
+++ movie_app.sql
@@ -3,7 +3,7 @@ CREATE DATABASE movie_app;
 
 -- Run SQL Query in movie_app DB
 CREATE TABLE movies (
-  id int(11) PRIMARY KEY AUTO_INCREMENT,
+  id varchar(10) PRIMARY KEY AUTO_INCREMENT,
   title varchar(255) NOT NULL,
   year year(4) DEFAULT NULL,
   rating float DEFAULT NULL,
