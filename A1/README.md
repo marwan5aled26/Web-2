b69f3308c8ced02be806b889a3aa72d6e2f0ea32
Marwan Khaled <merokhaled708@gmail.com>
2026-05-02 16:24:50 +0300

    Add files via upload

---
+++ A1/README.md
@@ -0,0 +1,1 @@
+# Web-Based-IS-Project
---
+++ A1/movie_app.sql
@@ -0,0 +1,13 @@
+-- Run SQL Query in New DB
+CREATE DATABASE movie_app;
+
+-- Run SQL Query in movie_app DB
+CREATE TABLE movies (
+  id varchar(20) PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  year varchar(10) DEFAULT NULL,
+  rating float DEFAULT NULL,
+  note text DEFAULT NULL,
+  poster varchar(255) DEFAULT NULL,
+  created_at timestamp NOT NULL DEFAULT current_timestamp()
+)
