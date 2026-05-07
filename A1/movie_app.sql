76c116324ce1bf2b523f8517a1bd849be0a2e407
Marwan Khaled <merokhaled708@gmail.com>
2026-05-07 10:47:16 +0300

    Update movie_app.sql

--- A1/movie_app.sql
+++ A1/movie_app.sql
@@ -1,13 +1,63 @@
--- Run SQL Query in New DB
-CREATE DATABASE movie_app;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2026 at 09:45 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
--- Run SQL Query in movie_app DB
-CREATE TABLE movies (
-  id varchar(20) PRIMARY KEY,
-  title varchar(255) NOT NULL,
-  year varchar(10) DEFAULT NULL,
-  rating float DEFAULT NULL,
-  note text DEFAULT NULL,
-  poster varchar(255) DEFAULT NULL,
-  created_at timestamp NOT NULL DEFAULT current_timestamp()
-)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `movie_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` varchar(20) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `year` varchar(10) DEFAULT NULL,
+  `rating` float DEFAULT NULL,
+  `note` text DEFAULT NULL,
+  `poster` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `movies`
+--
+
+INSERT INTO `movies` (`id`, `title`, `year`, `rating`, `note`, `poster`, `created_at`) VALUES
+('tt0068646', 'The Godfather', '1972', 0, '', 'https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_QL75_UY562_CR8,0,380,562_.jpg', '2026-04-26 13:58:11'),
+('tt0071562', 'The Godfather Part II', '1974', 0, '', 'https://m.media-amazon.com/images/M/MV5BMDIxMzBlZDktZjMxNy00ZGI4LTgxNDEtYWRlNzRjMjJmOGQ1XkEyXkFqcGc@._V1_QL75_UX380_CR0,4,380,562_.jpg', '2026-04-26 21:20:43'),
+('tt0111161', 'The Shawshank Redemption', '1994', 0, 'svava a', 'https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_QL75_UX380_CR0,4,380,562_.jpg', '2026-04-26 15:04:05'),
+('tt16311594', 'F1: The Movie', '2025', 0, 'Great Movie', 'https://m.media-amazon.com/images/M/MV5BNGI0MDI4NjEtOWU3ZS00ODQyLWFhYTgtNGYxM2ZkM2Q2YjE3XkEyXkFqcGc@._V1_QL75_UX380_CR0,0,380,562_.jpg', '2026-04-24 15:56:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
